82485ed942b6120c55fa3f8328f3eba4ebe26581
AuroraS0728 <AuroraS0728@users.noreply.github.com>
2026-04-19 10:26:04 +0800

    新增车辆表 Mapper。

---
+++ backend/src/main/java/com/smartcommunity/mapper/UserVehicleMapper.java
@@ -0,0 +1,9 @@
+package com.smartcommunity.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.smartcommunity.entity.UserVehicle;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserVehicleMapper extends BaseMapper<UserVehicle> {
+}
